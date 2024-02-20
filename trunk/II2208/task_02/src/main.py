2d9c57c0e966e6b1b68289374093ac32801b4980
Corowka <107122474+Corowka@users.noreply.github.com>
2024-02-20 11:06:18 +0300

    add task 2

--- trunk/II2208/task_02/src/main.py
+++ trunk/II2208/task_02/src/main.py
@@ -8,45 +8,32 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-
         self.contacts = []
-
         self.setWindowTitle("Contacts")
         self.setWindowIcon(QIcon("icon.ico"))
-
         self.layout = QGridLayout()
-
         self.label_name = QLabel("Name:")
         self.label_name.setFixedWidth(60)
-
         self.label_address = QLabel("Address:")
         self.label_address.setFixedWidth(60)
-
         self.input_name = QLineEdit()
-
+        self.input_name.textChanged.connect(self.on_text_changed)
         self.input_address = QLineEdit()
-
+        self.input_address.textChanged.connect(self.on_text_changed)
         self.button_add = QPushButton("Add")
         self.button_add.clicked.connect(self.add_contact)
-
         self.button_edit = QPushButton("Edit")
         self.button_edit.clicked.connect(self.edit_contact)
-
         self.button_remove = QPushButton("Remove")
         self.button_remove.clicked.connect(self.remove_contact)
-
         self.button_save = QPushButton("Save")
         self.button_save.clicked.connect(self.save_contacts)
-
         self.button_load = QPushButton("Load")
         self.button_load.clicked.connect(self.load_contacts)
-
         self.button_export = QPushButton("Export")
         self.button_export.clicked.connect(self.export_contacts)
-
         self.list = QListWidget()
         self.list.itemClicked.connect(self.select_contact)
-
         self.layout.addWidget(self.label_name, 0, 0)
         self.layout.addWidget(self.input_name, 0, 1)
         self.layout.addWidget(self.label_address, 1, 0)
@@ -58,13 +45,20 @@ class MainWindow(QMainWindow):
         self.layout.addWidget(self.button_load, 6, 0)
         self.layout.addWidget(self.button_export, 7, 0)
         self.layout.addWidget(self.list, 2, 1, 6, 1)
-
         self.setLayout(self.layout)
-
         self.container = QWidget()
         self.container.setLayout(self.layout)
-
         self.setCentralWidget(self.container)
+
+    def on_text_changed(self):
+        name = self.input_name.text()
+        address = self.input_address.text()
+        self.list.clear()
+        for i, c in enumerate(self.contacts):
+            if name == "" and address == "":
+                self.list.addItem(f"{self.contacts[i]['name']} - {self.contacts[i]['address']}")
+            elif name in self.contacts[i]["name"] and address in self.contacts[i]["address"]:
+                self.list.addItem(f"{self.contacts[i]['name']} - {self.contacts[i]['address']}")
 
     def add_contact(self):
         name = self.input_name.text()
@@ -80,7 +74,6 @@ class MainWindow(QMainWindow):
         selected_item = self.list.currentItem()
         if selected_item:
             index = self.list.row(selected_item)
-            name, address = self.contacts[index]["name"], self.contacts[index]["address"]
             self.contacts[index] = {"name": self.input_name.text(), "address": self.input_address.text()}
             self.list.currentItem().setText(f"{self.input_name.text()} - {self.input_address.text()}")
             self.clear_fields()
@@ -143,12 +136,12 @@ class MainWindow(QMainWindow):
 
             if fileName:
                 with open(fileName, "w") as file:
-                    file.write(f"BEGIN:VCARD\n")
-                    file.write(f"VERSION:3.0\n")
-                    file.write(f"N:;;;;\n")
+                    file.write("BEGIN:VCARD\n")
+                    file.write("VERSION:3.0\n")
+                    file.write("N:;;;;\n")
                     file.write(f"FN:{name}\n")
                     file.write(f"ADR:{address}\n")
-                    file.write(f"END:VCARD\n")
+                    file.write("END:VCARD\n")
                 print("vCard created!")
 
     def select_contact(self):
