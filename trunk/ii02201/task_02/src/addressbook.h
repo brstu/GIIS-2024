9b64e71bb742ec099d5e8a30b99ae045c3853638
Boreysha Oleg <boreysha.oleg@gmail.com>
2024-03-01 13:56:13 +0300

    Added task_02

---
+++ trunk/ii02201/task_02/src/addressbook.h
@@ -0,0 +1,57 @@
+#ifndef ADDRESSBOOK_H
+#define ADDRESSBOOK_H
+
+#include <QWidget>
+#include <QMessageBox>
+#include <QFile>
+#include <QFileDialog>
+#include "finddialog.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class AddressBook;
+}
+QT_END_NAMESPACE
+
+class AddressBook : public QWidget
+{
+    Q_OBJECT
+
+public:
+    AddressBook(QWidget *parent = nullptr);
+    ~AddressBook();
+
+private slots:
+    void on_addButton_clicked();
+
+    void on_submitButton_clicked();
+
+    void on_cancelButton_clicked();
+
+    void on_nextButton_clicked();
+
+    void on_prevButton_clicked();
+
+    void on_editButton_clicked();
+
+    void on_removeButton_clicked();
+
+    void on_findButton_clicked();
+
+    void on_saveButton_clicked();
+
+    void on_loadButton_clicked();
+
+    void on_exportButton_clicked();
+
+private:
+    Ui::AddressBook *ui;
+    QMap<QString, QString> contacts;
+    QString oldName;
+    QString oldAddress;
+    enum Mode { NavigationMode, AddingMode, EditingMode };
+    void updateInterface(Mode mode);
+    Mode currentMode;
+    FindDialog *dialog;
+};
+#endif // ADDRESSBOOK_H
