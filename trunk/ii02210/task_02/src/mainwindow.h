b501620fef50aeb7f1173f6933912d458029d5a7
Nika Levanevskaya <98054882+neonchikCallMe@users.noreply.github.com>
2024-03-05 13:01:12 +0300

    Update mainwindow.h

--- trunk/ii02210/task_02/src/mainwindow.h
+++ trunk/ii02210/task_02/src/mainwindow.h
@@ -15,7 +15,7 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    ~MainWindow() override;
 
 private slots:
     void on_pushButton_clicked();
