d1b5f12b56577f857bbd85e1e44ebd14665fa837
Corowka <107122474+Corowka@users.noreply.github.com>
2024-03-05 14:27:37 +0300

    lab_02 good

--- trunk/ii02208/task_02/src/main.py
+++ trunk/ii02208/task_02/src/main.py
@@ -17,14 +17,14 @@ class MainWindow(QMainWindow):
         self.label_address = QLabel("Address:")
         self.label_address.setFixedWidth(60)
         self.input_name = QLineEdit()
-        self.input_name.textChanged.connect(self.on_text_changed)
         self.input_address = QLineEdit()
-        self.input_address.textChanged.connect(self.on_text_changed)
         self.button_add = QPushButton("Add")
         self.button_add.clicked.connect(self.add_contact)
         self.button_edit = QPushButton("Edit")
         self.button_edit.clicked.connect(self.edit_contact)
         self.button_remove = QPushButton("Remove")
+        self.button_find = QPushButton("Find")
+        self.button_find.clicked.connect(self.find_contact)
         self.button_remove.clicked.connect(self.remove_contact)
         self.button_save = QPushButton("Save")
         self.button_save.clicked.connect(self.save_contacts)
@@ -41,23 +41,23 @@ class MainWindow(QMainWindow):
         self.layout.addWidget(self.button_add, 2, 0)
         self.layout.addWidget(self.button_edit, 3, 0)
         self.layout.addWidget(self.button_remove, 4, 0)
-        self.layout.addWidget(self.button_save, 5, 0)
-        self.layout.addWidget(self.button_load, 6, 0)
-        self.layout.addWidget(self.button_export, 7, 0)
-        self.layout.addWidget(self.list, 2, 1, 6, 1)
+        self.layout.addWidget(self.button_find, 5, 0)
+        self.layout.addWidget(self.button_save, 6, 0)
+        self.layout.addWidget(self.button_load, 7, 0)
+        self.layout.addWidget(self.button_export, 8, 0)
+        self.layout.addWidget(self.list, 2, 1, 7, 1)
         self.setLayout(self.layout)
         self.container = QWidget()
         self.container.setLayout(self.layout)
         self.setCentralWidget(self.container)
 
-    def on_text_changed(self):
+    def find_contact(self):
         name = self.input_name.text()
         address = self.input_address.text()
         self.list.clear()
-        for i in self.contacts:
-            if name == "" and address == "" or name in self.contacts[i]["name"] and address in self.contacts[i]["address"]:
+        for i in range(len(self.contacts)):
+            if name == "" and address == "" or name in self.contacts[i]["name"] and address in self.contacts[i]['address']:
                 self.list.addItem(f"{self.contacts[i]['name']} - {self.contacts[i]['address']}")
-
     def add_contact(self):
         name = self.input_name.text()
         address = self.input_address.text()
