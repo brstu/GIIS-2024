503ed5df9bd97e8adf055e030142f6d1c39817ef
Nika Levanevskaya <98054882+neonchikCallMe@users.noreply.github.com>
2024-04-02 14:34:28 +0300

    Update Block.cs

--- trunk/ii02210/task_04/src/Block.cs
+++ trunk/ii02210/task_04/src/Block.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,18 +15,18 @@ namespace tetris_
         public abstract int Id { get; }
 
         private int rotationState;
-        private Position offset;
+        private Position @readonly;
 
         public Block()
         {
-            offset = new Position(StartOffset.Row, StartOffset.Column);
+            @readonly = new Position(StartOffset.Row, StartOffset.Column);
         }
 
         public IEnumerable<Position> TilePositions()
         {
             foreach (Position p in Tiles[rotationState]) 
             {
-                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+                yield return new Position(p.Row + @readonly.Row, p.Column + @readonly.Column);
             }
         }
 
@@ -37,27 +37,21 @@ namespace tetris_
 
         public void RotateCCW()
         {
-            if (rotationState == 0)
-            { 
-                rotationState = (rotationState + 4 - 1) % 4;
-            }
-            else 
-            {
-                rotationState--;
-            }
+            if (rotationState == 0) rotationState = (rotationState + 4 - 1) % 4;
+            else rotationState--;
         }
 
         public void Move(int rows,  int columns)
         {
-            offset.Row += rows;
-            offset.Column += columns;
+            @readonly.Row += rows;
+            @readonly.Column += columns;
         }
 
         public void Reset()
         {
             rotationState = 0;
-            offset.Row = StartOffset.Row;
-            offset.Column = StartOffset.Column;
+            @readonly.Row = StartOffset.Row;
+            @readonly.Column = StartOffset.Column;
         }
     }
 }
