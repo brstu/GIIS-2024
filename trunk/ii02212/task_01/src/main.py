4c9fc9d4d404f16fad7aa76115d7514997407a23
snepone <llyubchuk14@mail.ru>
2024-05-13 12:45:56 +0300

    Task_01

--- trunk/ii02212/task_01/src/main.py
+++ trunk/ii02212/task_01/src/main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageTk
-import random
+from PIL import Image, ImageTks
 import math
+import secrets
 
 
 class ImageProcessingApp:
